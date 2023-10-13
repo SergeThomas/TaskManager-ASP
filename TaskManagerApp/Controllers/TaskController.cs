207ab38ee964469363b880c4a40300e29a0e6be8
Serge Thomas <smughemuzi@gmail.com>
2023-10-13 20:07:29 +0200

    Adding Delete functionality

--- TaskManagerApp/Controllers/TaskController.cs
+++ TaskManagerApp/Controllers/TaskController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using TaskManagerApp.Data;
 using TaskManagerApp.Models;
 
@@ -81,7 +82,47 @@ namespace TaskManagerApp.Controllers
 
             if (ModelState.IsValid)     // validating whether model is valid
             {
-                _db.TaskMans.Update(obj);      // this will add the new form to the table 
+                _db.TaskMans.Update(obj);      // method to update record based on primary key
+                _db.SaveChanges();          // this will push it to the database
+                return RedirectToAction("Index");       // redirect to the list page
+            }
+            return View(obj);
+        }
+
+
+        // this is the delete action method
+        // GET - this will get the task and display it using the task number since it is the primary key
+        public IActionResult Delete(int? taskNumber)
+        {
+            // return task if taskNumber found
+            if (taskNumber == null || taskNumber == 0)
+            {
+                return NotFound();
+            }
+            var taskFromDb = _db.TaskMans.Find(taskNumber);     // it will find the task based of it's taskNumber and assign to the variable
+            //var taskFromDb = _db.TaskMans.FirstOrDefault(u => u.TaskNumber == taskNumber);
+
+            if (taskFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(taskFromDb);    // if task is found, then return to view
+        }
+
+        // POST - this will delete the obj from the database
+        [HttpPost]      // always add httpPost when posting data
+        [ValidateAntiForgeryToken]      // this will prevent cross site forgery on form
+        public IActionResult Delete(TaskMan obj)
+        {
+            if (obj.DueDate.Date.ToString() == obj.DateAdded.Date.ToString())
+            {
+                ModelState.AddModelError("DueDate", "Due date can not be the same as current date (task must be assigned 24hrs in advance)");
+            }
+
+            if (ModelState.IsValid)     // validating whether model is valid
+            {
+                _db.TaskMans.Remove(obj);      // method to update record based on primary key
                 _db.SaveChanges();          // this will push it to the database
                 return RedirectToAction("Index");       // redirect to the list page
             }
